b233f13959134bff9326db2fada1a9a6b4765715
UmHyanho <163824092+umyunho@users.noreply.github.com>
2025-07-03 16:24:01 +0900

    feat: menuDao change

--- FirstProject/src/com/firstproject/cooook/dao/MenuDao.java
+++ FirstProject/src/com/firstproject/cooook/dao/MenuDao.java
@@ -1,45 +1,15 @@
 package com.firstproject.cooook.dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;  
-import java.util.ArrayList;
-import java.util.List;
 
-import com.firstproject.cooook.db.DBUtil;
-import com.firstproject.cooook.vo.MenuVO;
+import com.firstproject.orders.db.DBUtil;
 
 public class MenuDao {
-    
-    DBUtil du = new DBUtil();
-    
-    public List<MenuVO> getAllMenu(int categoryId) {
-        List<MenuVO> mList = new ArrayList<>();
-        Connection con = null;
-        try {
-            con = du.getConnection();
-
-            String sql = "SELECT menu_name, menu_id, category_id FROM menu WHERE category_id = ?";
-            PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, categoryId);
-            ResultSet rs = pstmt.executeQuery();
-
-            while (rs.next()) {
-                MenuVO mvo = new MenuVO();
-                mvo.setCategoryId(rs.getInt("category_id"));
-                mvo.setMenuID(rs.getInt("menu_id"));
-                mvo.setMenuName(rs.getString("menu_name"));
-                mList.add(mvo);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            du.close(con);
-        }
-
-        return mList;
-    }
-
-} 
+	public static void main(String[] args) {
+		
+		public 
+		Connection con = DBUtil.getConnection();
+		String sql = "SELECT menu_id, menu_name,";
+		
+	}
+}
