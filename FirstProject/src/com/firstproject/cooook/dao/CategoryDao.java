e0dbeaa6bdac8c85352a3d3c562ad8840015a796
Geonho Kim <yorjsgh@naver.com>
2025-07-07 14:17:53 +0900

    feat : 카테고리 기능 수정 (예외처리 보완 및 성능 개선)

--- FirstProject/src/com/firstproject/cooook/dao/CategoryDao.java
+++ FirstProject/src/com/firstproject/cooook/dao/CategoryDao.java
@@ -5,9 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.firstproject.cooook.db.DBUtil;
 import com.firstproject.cooook.vo.CategoryVO;
@@ -18,44 +16,49 @@ public class CategoryDao {
 		
 		String sql = """
 				SELECT 
-				    category_id									AS categoryId,
-				    category_name || '(' || category_ID || ')'	AS categoryName,
-				    parent_id									AS parentId
+					category_id 	AS categoryId, 
+					category_name 	AS categoryName,
+					parent_id 		AS parentId, 
+					level
 				FROM category
-				ORDER BY category_id				
+				START WITH parent_id IS NULL
+				CONNECT BY PRIOR category_id = parent_id
+				ORDER SIBLINGS BY category_id				
 				""";
 		
 		try (Connection connection = DBUtil.getConnection();
-				PreparedStatement statement = connection.prepareStatement(sql)) {
-			ResultSet resultSet = statement.executeQuery();
-
+				PreparedStatement statement = connection.prepareStatement(sql);
+				ResultSet resultSet = statement.executeQuery()) {
+			
 			while (resultSet.next()) {
 				CategoryVO category = new CategoryVO();
 				
 				category.setCategoryId(resultSet.getInt("categoryId"));
 				category.setCategoryName(resultSet.getString("categoryName"));
 				category.setParentId(resultSet.getObject("parentId", Integer.class));
+				category.setLevel(resultSet.getInt("level"));
 				
 				categories.add(category);
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
-		return createTreeCategory(categories);
+		return categories;
 	}
-	
-	public List<CategoryVO> selectCategory(int categoryId) {
+
+	public List<CategoryVO> selectChildCategory(int categoryId) {
 		List<CategoryVO> categories = new ArrayList<>();
 		
 		String sql = """
-			     SELECT 
-			     	category_id									AS categoryId,
-			    	category_name || '(' || category_ID || ')'	AS categoryName,
-			    	parent_id									AS parentId
-			     FROM category
-			     START WITH category_id = ?
-			     CONNECT BY PRIOR category_id = parent_id				
+				SELECT 
+					category_id 	AS categoryId, 
+					category_name 	AS categoryName,
+					parent_id 		AS parentId, 
+					level
+				FROM category
+				START WITH category_id = ?
+				CONNECT BY PRIOR category_id = parent_id
+				ORDER SIBLINGS BY category_id					
 			""";
 		
 		try (Connection connection = DBUtil.getConnection();
@@ -70,17 +73,48 @@ public class CategoryDao {
 				category.setCategoryId(resultSet.getInt("categoryId"));
 				category.setCategoryName(resultSet.getString("categoryName"));
 				category.setParentId(resultSet.getObject("parentId", Integer.class));
+				category.setLevel(resultSet.getInt("level"));
 				
 				categories.add(category);
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new RuntimeException(e);		
-		}				
-		return createTreeCategory(categories);
+		}
+		return categories;
 	}
 	
-	public void insertCategory(CategoryVO category) {
+	public CategoryVO selectCategoryById(int categoryId) {
+		String sql = """
+				SELECT 
+					category_id 	AS categoryId,
+					category_name 	AS categoryName,
+					parent_id 		AS parentId
+				FROM category
+				WHERE category_id = ?
+				""";
+		try (Connection connection = DBUtil.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql)){
+			
+			statement.setInt(1, categoryId);
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				CategoryVO category = new CategoryVO();
+
+				category.setCategoryId(resultSet.getInt("categoryId"));
+				category.setCategoryName(resultSet.getString("categoryName"));
+				category.setParentId(resultSet.getObject("parentID", Integer.class));
+
+				return category;
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+	
+	public int insertCategory(CategoryVO category) {
 		String sql = """
 				INSERT INTO category VALUES (
 				    CATEGORY_SEQ.NEXTVAL, 
@@ -92,31 +126,20 @@ public class CategoryDao {
 				""";		
 		try (Connection connection = DBUtil.getConnection();
 				PreparedStatement statement = connection.prepareStatement(sql)) {
+
 			statement.setString(1, category.getCategoryName());
 			statement.setInt(2, category.getParentId());
 			
-			statement.executeUpdate();
+			return statement.executeUpdate();
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new RuntimeException(e);			
 		}
 	}
 
-	public void updateCategory(CategoryVO category, int updateType) {
-		String sql = "";
-		if (updateType == 0) {
-			sql = """
-			UPDATE category
-			SET category_name = ?
-			WHERE category_id = ?
-			""";
-		} else {
-			sql = """
-			UPDATE category
-			SET parent_id = ?
-			WHERE category_id = ?
-			""";
-		}
+	public boolean updateCategory(CategoryVO category, int updateType) {
+		String sql = "UPDATE category SET ";
+		sql += updateType == 0 ? "category_name = ? " : "parent_id = ?";
+		sql += " WHERE category_id = ?";
 		
 		try (Connection connection = DBUtil.getConnection();
 				PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -127,14 +150,13 @@ public class CategoryDao {
 			}
 			statement.setInt(2, category.getCategoryId());
 			
-			statement.executeUpdate();
+			return statement.executeUpdate() > 0;
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new RuntimeException(e);			
 		}
 	}
 
-	public void deleteCategory(int categoryId) {
+	public boolean deleteCategory(int categoryId) {
 		String sql = """
 				DELETE FROM category
 				WHERE category_id IN (
@@ -145,42 +167,14 @@ public class CategoryDao {
 				""";
 		try (Connection connection = DBUtil.getConnection();
 				PreparedStatement statement = connection.prepareStatement(sql)) {
-			statement.setInt(1, categoryId);			
-			statement.executeUpdate();
+
+			statement.setInt(1, categoryId);
+			return statement.executeUpdate() > 0;
 		} catch (SQLException e) {
-			e.printStackTrace();			
 			throw new RuntimeException(e);			
 		}
 	}
 
-	
-	private List<CategoryVO> createTreeCategory(List<CategoryVO> categories) {
-		Map<Integer, CategoryVO> categoryMap = new HashMap<>();
-		List<CategoryVO> roots = new ArrayList<>();
-		
-		for (CategoryVO category : categories) {
-			categoryMap.put(category.getCategoryId(), category);
-		}
-		
-		for (CategoryVO category : categories) {
-			Integer parentId = category.getParentId();
-			
-			if (parentId == null || parentId == 0) {
-				roots.add(category);
-				continue;
-			} 
-
-	        CategoryVO parent = categoryMap.get(parentId);
-
-	        if (parent != null) {
-	            parent.getChild().add(category);
-	        } else {
-	            roots.add(category);
-	        }
-		}
-		
-		return roots;
-	}
 	
 	public List<CategoryVO> selectLeafCategories() {
 	    List<CategoryVO> list = new ArrayList<>();
@@ -224,7 +218,5 @@ public class CategoryDao {
 	            flatten(c.getChild(), flat);
 	        }
 	    }
-	}
-
-	
+	}        
 }
